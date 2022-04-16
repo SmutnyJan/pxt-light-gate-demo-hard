b8882d0c5bca7d0efbe9182de617dfcac42ba8a9
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 13:02:52 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let jeHraZapnuta = false
+input.onButtonPressed(Button.A, function () {
+    if (jeHraZapnuta == false) {
+        lightGate.calibrate(Deviation.Medium)
+    }
+    jeHraZapnuta = !(jeHraZapnuta)
+})
+lightGate.onLightLevelDrop(function () {
+    if (jeHraZapnuta) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+})
